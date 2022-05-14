e2cc94ea320bc3986dbf975b5f57aad570ffd07a
Maxime Vaude <mvaude@users.noreply.github.com>
2022-05-14 02:29:12 +0200

    feat: create issue from todo file (#13)

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,92 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as fs from 'fs'
+import * as util from 'util'
+import {inspect} from 'util'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const inputs = {
+      token: core.getInput('token'),
+      repository: core.getInput('repository'),
+      contentFilepath: core.getInput('content-filepath')
+    }
+    core.debug(`Inputs: ${inspect(inputs)}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const [owner, repo] = inputs.repository.split('/')
+    core.debug(`Repo: ${inspect(repo)}`)
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const octokit = github.getOctokit(inputs.token)
+    const issues: number[] = []
+
+    // check file exists
+    if (await util.promisify(fs.exists)(inputs.contentFilepath)) {
+      // fetch file
+      const fileContent = await fs.promises.readFile(inputs.contentFilepath, {
+        encoding: 'utf8'
+      })
+
+      for (const issueRaw of fileContent.split(/\n/)) {
+        // get title and label
+        const [label, title] = issueRaw.split(/:/)
+
+        const issueNumber = await (async (): Promise<number> => {
+          // TODO:feat:update existing issue
+          // if (inputs.issueNumber) {
+          //   await octokit.rest.issues.update({
+          //     owner: owner,
+          //     repo: repo,
+          //     issue_number: inputs.issueNumber,
+          //     title: inputs.title,
+          //     body: fileContent
+          //   })
+          //   core.info(`Updated issue #${inputs.issueNumber}`)
+
+          //   return inputs.issueNumber
+          // }
+          // create issue
+          const {data: issue} = await octokit.rest.issues.create({
+            owner,
+            repo,
+            title,
+            body: ':rocket:'
+          })
+          core.info(`Created issue #${issue.number}`)
+
+          return issue.number
+        })()
+
+        core.info(`Applying label '${label}'`)
+        await octokit.rest.issues.addLabels({
+          owner,
+          repo,
+          issue_number: issueNumber,
+          labels: [label]
+        })
+        issues.push(issueNumber)
+      }
+
+      // TODO: apply assignees
+      // if (inputs.assignees.length > 0) {
+      //   core.info(`Applying assignees '${inputs.assignees}'`)
+      //   await octokit.rest.issues.addAssignees({
+      //     owner: owner,
+      //     repo: repo,
+      //     issue_number: issueNumber,
+      //     assignees: inputs.assignees
+      //   })
+      // }
+
+      // set output
+      core.setOutput('issues', issues.join(','))
+    } else {
+      new Error(`File not found at path '${inputs.contentFilepath}'`)
+    }
+  } catch (error: Error | unknown) {
+    if (error instanceof Error) {
+      core.debug(inspect(error))
+      core.setFailed(error.message)
+    }
   }
 }
 
